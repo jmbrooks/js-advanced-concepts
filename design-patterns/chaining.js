66017b1da0db66717a917826506a6c8b7eefaaea
jmbrooks <jbrooks93@gmail.com>
2017-04-02 23:39:55 -0700

    first commit, chaining

---
+++ design-patterns/chaining.js
@@ -0,0 +1,28 @@
+/** CHAINING PATTERN
+ * Allows you to call methods on an object one after the other,
+ * without returning the previous operations values,
+ * or spittling method calls onto multiple lines.
+ *
+ * Benefits: Concise, readable, encourages specialize functions
+ * Drawbacks: Harder to debug, some dismiss as not worth using
+ */
+
+// Example of chaining pattern implementation
+var myObj = {
+	myName: 'johnathan brooks',
+	reverse: function () {
+		this.myName = this.myName.split('').reverse().join('');
+        return this;
+	},
+    update: function () {
+    	this.myName = 'faceless hacker';
+    	return this;
+    },
+    read: function () {
+    	return 'my current username is: ' + this.myName;
+    }
+}
+
+console.log(myObj.update().reverse().read());
+// Here's the same task WITHOUT chaining\
+
